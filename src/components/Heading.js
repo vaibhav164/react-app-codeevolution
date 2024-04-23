af66fc141ac41210644debe24e820c427672af26
vaibhav164 <vai1641996@gmail.com>
2024-04-24 04:28:59 +0530

    Converted Heading funcional component to class component

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,4 +1,8 @@
-// import React from "react";
-export function Heading() {
-  return <h3>Hello Vaibhav</h3>;
+import React from "react";
+
+class Heading extends React.Component {
+  render() {
+    return <h3>Hello Vaibhav</h3>;
+  }
 }
+export { Heading };
