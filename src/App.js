435f37816235d39393ac2c313b1ccc8eee1c95b8
vaibhav164 <vai1641996@gmail.com>
2024-04-25 04:36:43 +0530

    setting up App.js for useReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
 import "./App.css";
-import { Heading } from "./components/Heading";
 
-export const UserContext = React.createContext();
-export const SecondContext = React.createContext();
 const App = () => {
-  return (
-    <div className="App">
-      <UserContext.Provider value={"Vaibhav's first Context"}>
-        <SecondContext.Provider value={"second context Value"}>
-          <Heading />
-        </SecondContext.Provider>
-      </UserContext.Provider>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
