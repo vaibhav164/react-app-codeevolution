190bbb47a57d89baa1e551232c9a0227999d0a6f
vaibhav164 <vai1641996@gmail.com>
2024-04-25 03:39:48 +0530

    Demonstrated that how to use context using provider and consumer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import "./App.css";
 import { Heading } from "./components/Heading";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
+export const userContext = React.createContext();
+const App = () => {
+  return (
+    <div className="App">
+      <userContext.Provider value="Vaibhav's first Context">
         <Heading />
-      </div>
-    );
-  }
-}
+      </userContext.Provider>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Component2.js
+++ src/components/Component2.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import { userContext } from "../App";
 function Component2() {
-  return <div>Component2</div>;
+  return (
+    <userContext.Consumer>
+      {(value) => <div>Data for context vlaue is {value}</div>}
+    </userContext.Consumer>
+  );
 }
 
 export default Component2;
