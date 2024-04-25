7daee37d2924bec420947b885ae48d708ace9d9a
vaibhav164 <vai1641996@gmail.com>
2024-04-25 15:57:51 +0530

    Learned that first the file loads after that jsx component and then the useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-
+import CounterComponent from "./components/CounterComponent";
+console.log("file_Log");
 const App = () => {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("Effect Log");
+  });
+  return (
+    <div className="App">
+      {/* <p>{count}</p> */}
+      <CounterComponent />
+    </div>
+  );
 };
 
 export default App;
